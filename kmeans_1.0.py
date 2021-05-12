890dc195fc707274bf5772c70881a7156a233611
‪Ofri Streichman‬‏ <74958421+Ofri-Streichman@users.noreply.github.com>
2021-05-12 10:29:28 +0100

    Update kmeans_1.0.py

--- kmeans_1.0.py
+++ kmeans_1.0.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import argparse
 
 # This function reads from the commend line.
@@ -13,3 +15,55 @@ K = args.K
 N = args.N
 d = args.d
 MAX_ITER = args.MAX_ITER
+
+#________________________________________________________#
+
+# reading the data from the file and putting n vectors in an array
+data = []
+while (True):
+    try:
+        vec = []
+        for num in input().split(','):
+            vec.append(float(num))
+        data.append(a)
+    except EOFError:
+        break
+
+# initiating our info into a 4 tables of size K for the K clusters, centroids, size and sums
+Clstr = [[]*k] #has k empty lists ehich represent the ckusters
+Global Cntr = []
+Cl_size = []
+Cl_sum = []
+
+# Start by filling the Centroids array with K random vectors (deterministically)
+for i in range(k):
+    Clstr[i] = data[i]
+
+#the functions we'll need:
+# calculating the difference between two vectors of length D
+def diff(vec1, vec2):
+    dif =0
+    for i in range(d):
+        dif += ((vec1[i])-(vac2[i]))**2
+    assert isinstance(d, int)
+    return dif**(1/d)
+
+# finding the right cluster for a vector of size d
+def place(vec):
+    min=[0,float('inf')]
+    for i in range(k):
+        dif= diff(vec, Clstr[i])
+        if (dif<min[1]):
+            min= [i, dif]
+    Clstr[min[0]].add(vec)
+    return
+
+
+
+
+#the main part of the function:
+int i=0
+Var: Bool = True
+#While ((i < MAX_ITER) and Var):
+
+
