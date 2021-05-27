cf4340500374e83b3bd9081497a646f504dde3ed
ValeriaVaisman <84072848+ValeriaVaisman@users.noreply.github.com>
2021-05-27 03:21:10 +0300

    Update kmeans.c

--- C/kmeans.c
+++ C/kmeans.c
@@ -4,21 +4,20 @@
 #include <string.h>
 
 typedef struct {
-    float * vector_sum;
+    double* vector_sum;
     int size;
-
 }cluster;
 
-cluster* cluster_init(cluster *, int,int);
-float* array_copy(float*,const float*,  int);
-void array_2dim_copy(float **, float **,int ,int );
-int array_equal(float** , float** , int ,int ) ;
-float diff(const float *,const float * ,int);
-int min(const float *, int );
-float** new_centroid(float **,  cluster* , int , int );
-void update_centroid(const float* , cluster*  , int, int );
-void reset_cluster(cluster * , int ,  int );
-void print_cents_array(float **,int ,int );
+double* array_copy(double*,const double*, int);
+void array_2dim_copy(double **, double **,int ,int );
+int array_equal(double** , double** , int ,int ) ;
+double diff(const double *,const double* ,int);
+int min(const double *, int);
+void update_centroid(const double* , cluster*  , int, int );
+double** new_centroid(double**,  cluster* , int , int );
+cluster* cluster_init(cluster*, int,int);
+void reset_cluster(cluster* , int, int );
+void print_cents_array(double **,int ,int );
 
 /*initializing clusters*/
 cluster* cluster_init(cluster * clusters, int k, int d){
@@ -26,7 +25,7 @@ cluster* cluster_init(cluster * clusters, int k, int d){
     for (j=0;j<k; j++){
         cluster c;
         c.size=0;
-        c.vector_sum = (float *) malloc(d * sizeof(float));
+        c.vector_sum = (double*) malloc(d * sizeof(double));
         assert(c.vector_sum != NULL);
         for (i=0;i<d; i++) {
             c.vector_sum[i] = 0;
@@ -37,30 +36,30 @@ cluster* cluster_init(cluster * clusters, int k, int d){
 }
 
 /*copy array to other array*/
-float* array_copy(float* arr1,const float* arr2,  int d){
+double* array_copy(double* array1,const double* array2,  int d){
     int k;
     for(k=0;k<d;k++){
-        arr1[k]=arr2[k];
+        array1[k]=array2[k];
     }
-    return arr1;
+    return array1;
 }
 
 /*copys 2d array to other 2d array*/
-void array_2dim_copy(float **pDouble, float **pDouble1,int K,int d) {
+void array_2dim_copy(double **Double, double **Double1,int K,int d) {
     int l,m;
     for(l=0;l<K;l++){
         for(m=0;m<d;m++){
-            pDouble[l][m]=pDouble1[l][m];
+            Double[l][m]=Double1[l][m];
         }
     }
 }
 
 /*checks if 2 arrays are equal*/
-int array_equal(float** prev, float** curr, int k,int d) { 
+int array_equal(double** prev_array, double** curr_array, int k,int d) {
     int p,r;
-    for ( p = 0; p < k; p++) {
+    for (p = 0; p < k; p++) {
         for (r = 0; r < d; r++) {
-            if (prev[p][r] != curr[p][r]) {
+            if (prev_array[p][r] != curr_array[p][r]) {
                 return 0;
             }
         }
@@ -69,52 +68,52 @@ int array_equal(float** prev, float** curr, int k,int d) {
 }
 
 /*returns the index of the min*/
-int min(const float *arr, int k) {
+int min(const double *array, int k) {
     int r, index;
-    float min;
+    double min;
     index = 0;
-    min = arr[0];
+    min = array[0];
     for(r=0;r<k;r++){
-        if (arr[r] < min){
+        if (array[r] < min){
             index = r;
-            min = arr[r];
+            min = array[r];
         }
     }
     return index;
 }
 
 /*calculates the distance between 2 vectors*/
-float diff(const float * vec1,const float * vec2,int d){
-    float sum;
+double diff(const double * vector1,const double * vector2,int d){
+    double sum;
     int j;
     sum=0;
     for (j=0;j<d;j++){
-        sum+=(vec1[j]-vec2[j])*(vec1[j]-vec2[j]);
+        sum+=(vector1[j]-vector2[j])*(vector1[j]-vector2[j]);
     }
     return sum;
 
 }
 /*updates the vector_sum field when new vector is added*/
-void update_centroid(const float* vec, cluster * clusters,int min_index, int d){
+void update_centroid(const double* vector, cluster * clusters,int min_index, int d){
     int q;
     for (q=0;q<d;q++){
-        clusters[min_index].vector_sum[q]+=vec[q];
+        clusters[min_index].vector_sum[q]+=vector[q];
     }
 
 }
 /*calculates the new centroids*/
-float** new_centroid(float **curr_cent,  cluster* clusters, int k, int d) {
+double** new_centroid(double **curr_cent,  cluster* clusters, int k, int d) {
     int i,j;
     for(i=0; i<k; i++){
         for (j=0;j<d;j++){
-            curr_cent[i][j] = clusters[i].vector_sum[j]/ (float)clusters[i].size;
+            curr_cent[i][j] = clusters[i].vector_sum[j]/ (double)clusters[i].size;
         }
     }
     return curr_cent;
 }
 
 /*resets the clusters by filling with 0s*/
-void reset_cluster(cluster * cluster1, int k,  int d) {
+void reset_cluster(cluster* cluster1, int k,  int d) {
     int x,y;
     for(y=0;y<k;y++) {
         cluster1[y].size=0;
@@ -124,14 +123,14 @@ void reset_cluster(cluster * cluster1, int k,  int d) {
     }
 }
 /*prints the centroides*/
-void print_cents_array(float **pDouble,int K,int d) {
+void print_cents_array(double** Double,int K,int d) {
     int x, y;
     for (x = 0; x < K; x++) {
         for (y = 0; y < d; y++) {
             if (y < d - 1) {
-                printf("%.4f,", pDouble[x][y]);
+                printf("%.4f,", Double[x][y]);
             } else {
-                printf("%.4f\n", pDouble[x][y]);
+                printf("%.4f\n", Double[x][y]);
             }
         }
     }
@@ -139,125 +138,121 @@ void print_cents_array(float **pDouble,int K,int d) {
 
 int main(int argc, char* argv[]) {
 
-    int K, MAX_ITER;
-    int d;
-	int N=0;
+    int K, max_iter;
+    int d = 0;
+    int N = 0;
     int cnt;
     int e,m;
     int r;
     int p;
-    int i;
+    int i=0;
     int x,b;
     int j;
-    int length=1;
+    int length = 1;
     const char delimiter[2] = ",";
     char* token;
-    char** strings_array;
-    char* vector_string=NULL;
-    
-    float **vectors_array;
+    char* token1;
+    char* first_line;
+    char* first_line_copy;
+    char* vector_string;
     cluster *clusters;
-    float **prev;
-    float **curr;
+    double **vectors_array;
+    double **prev_centroids;
+    double **curr_centroids;
     K = atoi(argv[1]);
-    MAX_ITER = atoi(argv[2]);
-    (void)argc;
-    clusters = (cluster *) malloc(K * sizeof(cluster));
-    strings_array= (char**) malloc(sizeof(char*));
-    vector_string= (char*) malloc(sizeof(char));
 
-    printf("%s","kaki");
-    while (scanf("%s", vector_string)==1) {
-        length++;
-        strings_array = (char **) realloc(strings_array, length * sizeof(char*));
-        strings_array[N] = (char *) malloc(sizeof(char *));
-        strcpy(strings_array[N],vector_string);
-        N++;
+    if(argc < 4) {
+        max_iter = 200;
+    }
+    else {
+        max_iter = atoi(argv[2]);
     }
 
+    first_line= (char*) malloc(200*sizeof(char*));
+    first_line_copy= (char*) malloc(200*sizeof(char*));
+    vector_string= (char*) malloc(200*sizeof(char*));
+    vectors_array = (double **) malloc( sizeof(double *));
+    clusters = (cluster*) malloc(K*sizeof (cluster));
+    prev_centroids = (double **) malloc(K * sizeof(double *));
+    curr_centroids = (double **) malloc(K * sizeof(double *));
 
-    vectors_array = (float **) malloc(N * sizeof(float *));
-    prev = (float **) malloc(K * sizeof(float *));
-    curr = (float **) malloc(K * sizeof(float *));
-    token = strtok(strings_array[0], delimiter);
+    scanf("%s", first_line);
+    strcpy(first_line_copy,first_line);
+    token = strtok(first_line, delimiter);
 
-    d = 0;
     while (token != NULL) {
         token = strtok(NULL, delimiter);
         d++;
     }
+    N++;
 
-    for (e = 0; e < N; e++) {
-        vectors_array[e] = (float *) malloc(d * sizeof(float));
-        assert(vectors_array[e] != NULL);
-    }
-
-    for (e = 0; e < K; e++) {
-        prev[e] = (float *) malloc(d * sizeof(float));
-        assert(prev[e] != NULL);
-        curr[e] = (float *) malloc(d * sizeof(float));
-        assert(prev[e] != NULL);
-        for(m=0;m<d;m++){
-            prev[e][m]=0;
-        }
-    }
-
-    i=0;
-    while (i < N) {
-        char* token1;
-        int tokenCoun;
-        token1 = strtok(strings_array[i], delimiter);
-        r = 0;
-        printf("%s","kaki");
-        vectors_array[i][r] = (float) atof(token1);
-        tokenCoun = 1;
-        while (tokenCoun < d) {
-            printf("%f",vectors_array[i][r]);
-            r++;
-            token1 = strtok(NULL, delimiter);
-            vectors_array[i][r] = (float) atof(token1);
-            printf("%f",vectors_array[i][r]);
-            tokenCoun++;
-        }
+    vectors_array[0] = (double*) malloc(d*sizeof (double));
+    token = strtok(first_line_copy, delimiter);
+    while (token != NULL) {
+        vectors_array[0][i] = (double) atof(token);
+        token = strtok(NULL, delimiter);
         i++;
     }
 
+    while (scanf("%s", vector_string) == 1) {
+        length++;
+        vectors_array =(double**) realloc(vectors_array, length*d*sizeof(double*));
+        vectors_array[N] = (double *) malloc(d * sizeof(double));
+        assert(vectors_array[N] != NULL);
+        token1 = strtok(vector_string, delimiter);/*split the string*/
+        r = 0;
+        vectors_array[N][r] = (double) atof(token1);
+        cnt = 1;
+        while (cnt < d) {
+            r++;
+            token1 = strtok(NULL, delimiter);
+            vectors_array[N][r] = (double) atof(token1);
+            cnt++;
+        }
+        N++;
+    }
+
     clusters = cluster_init(clusters,K,d);
+
+    for (e = 0; e < K; e++) {
+        prev_centroids[e] = (double *) malloc(d * sizeof(double));
+        assert(prev_centroids[e] != NULL);
+        curr_centroids[e] = (double *) malloc(d * sizeof(double));
+        assert(prev_centroids[e] != NULL);
+        for(m=0;m<d;m++){
+            prev_centroids[e][m]=0;
+        }
+    }
+
+
     for(j=0;j<K;j++) {
         clusters[j].vector_sum= array_copy(clusters[j].vector_sum, vectors_array[j], d);
-        curr[j]= array_copy(curr[j], vectors_array[j], d);
+        curr_centroids[j]= array_copy(curr_centroids[j], vectors_array[j], d);
     }
 
     cnt=0;
-    while(cnt<MAX_ITER && !array_equal(curr,prev,K,d)){
-        array_2dim_copy(prev,curr,K,d);
+    while(cnt < max_iter && !array_equal(curr_centroids,prev_centroids,K,d)){
+        array_2dim_copy(prev_centroids,curr_centroids,K,d);
         reset_cluster(clusters,K,d);
         for(x=0;x<N;x++) {
-            float *diff1;
+            double *distance;
             int min_index;
-            diff1 = (float *)malloc(K* sizeof(float));
+            distance = (double *)malloc(K* sizeof(double));
             for (b = 0; b < K; b++) {
-                diff1[b] = diff(vectors_array[x], prev[b], d);
+                distance[b] = diff(vectors_array[x], prev_centroids[b], d);
             }
-            min_index = min(diff1, K);
+            min_index = min(distance, K);
             update_centroid(vectors_array[x],clusters,min_index,d);
             clusters[min_index].size++;
         }
-        curr =new_centroid(curr,clusters,K,d);
+        curr_centroids =new_centroid(curr_centroids,clusters,K,d);
         cnt++;
     }
 
-    print_cents_array(curr,K,d);
+    print_cents_array(curr_centroids,K,d);
 
     free(vector_string);
     vector_string=NULL;
-
-    for (p=0 ; p < N; p++) {
-        free(strings_array[p]);
-        strings_array[p] = NULL;
-    }
-    free(strings_array);
-    strings_array=NULL;
 
     for (p=0 ; p < N; p++) {
         free(vectors_array[p]);
@@ -267,15 +262,15 @@ int main(int argc, char* argv[]) {
     vectors_array=NULL;
 
     for (p=0 ; p < K; p++) {
-        free(prev[p]);
-        free(curr[p]);
-        curr[p] = NULL;
-        prev[p] = NULL;
+        free(prev_centroids[p]);
+        free(curr_centroids[p]);
+        curr_centroids[p] = NULL;
+        prev_centroids[p] = NULL;
     }
-    free(prev);
-    prev=NULL;
-    free(curr);
-    curr=NULL;
+    free(prev_centroids);
+    prev_centroids=NULL;
+    free(curr_centroids);
+    curr_centroids=NULL;
 
 
     for (r=0;r<K; r++){
